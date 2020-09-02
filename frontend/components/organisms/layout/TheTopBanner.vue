441291c1abb24f1e575c27eefc10eb478c64a3ac
Michael J. McCawley <mmccawley@us.ibm.com>
2020-09-02 13:20:04 -0400

    new banner from DAM

--- frontend/components/organisms/layout/TheTopBanner.vue
+++ frontend/components/organisms/layout/TheTopBanner.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <v-parallax
-      src="/images/ibm-component-banner.png"
+      src="/images/aurora-banner.jpg"
       height="400"
       dark
     >
@@ -12,7 +12,7 @@
         class="white--text"
       >
         <v-flex text-right class="mr-5">
-          <img src="~/assets/icon.png" alt="doccano" height="200">
+          <img src="/images/solve-transp.png" alt="doccano" height="200">
         </v-flex>
         <v-flex>
           <h1 class="mb-2 display-1 text-xs-center">
